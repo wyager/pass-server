cbbf7a925db851cfbbae6c3648573fce55272d58
Will Yager <will.yager@gmail.com>
2018-02-25 00:38:15 -0500

    Server auth working

---
+++ gen-cert.sh
@@ -0,0 +1,1 @@
+openssl req -config req.cnf -new -nodes -out cert.pem -x509 -subj '/CN=localhost'
--- src/Main.hs
+++ src/Main.hs
@@ -6,34 +6,48 @@ import Control.Applicative ((<|>))
 -- PEM loading
 import qualified Data.X509 as X509
 import qualified Data.X509.CertificateStore as X509CS
+import qualified Data.X509.Memory as X509M
 import qualified Data.PEM as PEM
 -- Client
 import qualified Network.Wreq as Wreq
 import Control.Lens ((&), (.~))
 import qualified Network.HTTP.Client.TLS as TLSClient
 import qualified Network.Connection as Connection
-import Network.TLS as TLS
+import qualified Network.TLS as TLS
+import qualified Network.TLS.Extra as TLSE
 import Network.Socket (HostName)
 import Data.Default (def)
 -- Server
-import qualified Network.Wai.Handler.Warp
-import qualified Network.Wai.Handler.WarpTLS
-import qualified Network.Wai
+import qualified Network.Wai.Handler.Warp as Warp
+import qualified Network.Wai.Handler.WarpTLS as WarpTLS
+import qualified Network.Wai as Wai
+import qualified Network.HTTP.Types.Status as Status
+import qualified Network.HTTP.Types.Header as Header
 -- Command line
 import qualified Options.Applicative as Opt
 
 
+parseOnePem :: BS.ByteString -> Either String PEM.PEM
+parseOnePem bs = PEM.pemParseBS bs >>= \case
+    [] -> Left "Empty PEM data"
+    [x] -> return x
+    _ -> Left "Too many PEM sections"
+
+
 decodeCertPem :: BS.ByteString -> Either String X509.SignedCertificate
-decodeCertPem bs = do
-    pems <- PEM.pemParseBS bs
-    pem <- case pems of
-        [] -> Left "Empty PEM data"
-        [x] -> return x
-        _ -> Left "Too many PEM sections"
-    X509.decodeSignedObject (PEM.pemContent pem)
+decodeCertPem bs = parseOnePem bs >>= (X509.decodeSignedObject . PEM.pemContent)
+
+decodeKeyPem :: BS.ByteString -> Either String X509.PrivKey
+decodeKeyPem bs =  case X509M.readKeyFileFromMemory bs of
+    [] -> Left "No privkeys in file"
+    [p] -> return p
+    _ -> Left "Too many things in privkey file"
 
 loadCertPem :: FilePath -> IO (Either String X509.SignedCertificate)
 loadCertPem path = decodeCertPem <$> BS.readFile path
+
+loadKeyPem :: FilePath -> IO (Either String X509.PrivKey)
+loadKeyPem path = decodeKeyPem <$> BS.readFile path
 
 main :: IO ()
 main = do
@@ -45,7 +59,7 @@ main = do
 client :: FilePath -> TLS.HostName -> IO ()
 client certPath server = do
     cert <- either error id <$> loadCertPem certPath
-    res <- Wreq.getWith (managerWithCert server cert) ("http://" ++ server ++ "/")
+    res <- Wreq.getWith (managerWithCert server cert) ("https://" ++ server ++ "/")
     print res
 
 
@@ -55,21 +69,25 @@ managerWithCert hostname cert = Wreq.defaults & Wreq.manager .~ Left mgrSettings
     mgrSettings = TLSClient.mkManagerSettings tlsSettings Nothing
     tlsSettings = Connection.TLSSettings clientParams
     clientParams = TLS.ClientParams {
-        clientUseMaxFragmentLength = Nothing,
-        clientServerIdentification = (hostname, ""),
-        clientUseServerNameIndication = False,
-        clientWantSessionResume = Nothing,
-        clientShared = def {sharedCAStore = X509CS.makeCertificateStore [cert]},
+        TLS.clientUseMaxFragmentLength = Nothing,
+        TLS.clientServerIdentification = (hostname, ""),
+        TLS.clientUseServerNameIndication = False,
+        TLS.clientWantSessionResume = Nothing,
+        TLS.clientShared = def {TLS.sharedCAStore = X509CS.makeCertificateStore [cert]},
         -- NB: I think I need to modify clientHooks onServerCertificate to rule out anything besides [cert]
-        clientHooks = def, 
-        clientSupported = def,
-        clientDebug = def
+        TLS.clientHooks = def, 
+        TLS.clientSupported = def {TLS.supportedCiphers = TLSE.ciphersuite_strong},
+        TLS.clientDebug = def
     }
 
 server :: FilePath -> FilePath -> IO ()
-server certPath keyPath = do
-    cert <- either error id <$> loadCertPem certPath
+server certPath keyPath = WarpTLS.runTLS tlsSettings settings serverApp
+    where
+    tlsSettings =  WarpTLS.tlsSettings certPath keyPath
+    settings = Warp.defaultSettings
 
+serverApp :: Wai.Application
+serverApp req send = send (Wai.responseLBS Status.status200 [] "lookin' good")
 
 data Action
     = Client {serverCertPath :: FilePath, serverHost :: TLS.HostName}
@@ -87,7 +105,7 @@ action = client <|> server
     serverCertPath = Opt.long "cert-path" 
                   <> Opt.metavar "FILE.pem" 
                   <> Opt.help "The PEM file containing the server's TLS cert"
-                  <> Opt.value "certificate.pem"
+                  <> Opt.value "cert.pem"
                   <> Opt.showDefault
     serverKeyPath = Opt.long "key-path" 
                   <> Opt.metavar "FILE.pem" 
